cad7e605058e76ae7118a2e25ea30fde23922076
IfrahDar <91304978+IfrahDar@users.noreply.github.com>
2023-06-04 23:02:44 +0500

    Update dart_demo.dart

--- bin/dart_demo.dart
+++ bin/dart_demo.dart
@@ -1,8 +1,6 @@
 import 'dart:html';
 
 void main() {
-  querySelector('#container')?.text = 'Hello from your Dart App.';
-  // Calling the element with id educative and setting its value dynamically
   var myText = querySelector('#educative');
-  myText?.text = "Hello! Welcome to Educative.io";
+  myText?.text = "Must explore dart course on educative";
 }
